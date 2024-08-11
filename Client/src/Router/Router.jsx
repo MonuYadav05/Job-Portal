502eeca9e32bd267e295d567c21679ee9ab202f5
Monu Yadav <149148564+MonuYadav05@users.noreply.github.com>
2024-08-11 11:52:16 +0530

    Update Router.jsx

--- Client/src/Router/Router.jsx
+++ Client/src/Router/Router.jsx
@@ -19,22 +19,10 @@ const router = createBrowserRouter([S
       {
         path: "/edit-job/:id",
         element: <UpdateJob />,
-        loader: async ({ params }) => {
-  try {
-    const response = await fetch(
-      `https://job-portal-server-delta-flax.vercel.app/edit-job/${params.id}`
-    );
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
-
-    return response.json();
-  } catch (error) {
-    console.error("Error fetching job data:", error);
-    throw error;
-  }
-};
+             loader: ({ params }) =>
+          fetch(
+            `https://job-portal-server-delta-flax.vercel.app/edit-job/${params.id}`
+          ),
       },
       { path: "/job/:id", element: <JobDetail /> },
     ],
